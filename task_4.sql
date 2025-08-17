0061cd87359c2a8ed178844f416b4f85de95fa57
REBECCA-WONDU <your-email@example.com>
2025-08-17 13:11:50 +0300

    add task_4.sql script

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,5 @@
--- Prints full description of books table without using DESCRIBE/EXPLAIN
-SELECT 
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IF(IS_NULLABLE = 'YES', 'YES', 'NO') AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM 
-    INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-    TABLE_SCHEMA = DATABASE() 
-    AND TABLE_NAME = 'books'
-ORDER BY 
-    ORDINAL_POSITION;
+-- Prints the full description of the table books from the alx_book_store database
+-- Database name is passed as an argument to the mysql command
+-- Uses SHOW CREATE TABLE to display the table structure without DESCRIBE or EXPLAIN
+
+SHOW CREATE TABLE books;
