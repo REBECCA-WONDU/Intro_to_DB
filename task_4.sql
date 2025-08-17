31bcc4c0c2ca35208c15dd706329bcd73df2ca7c
REBECCA-WONDU <your-email@example.com>
2025-08-17 12:29:39 +0300

    add task_4.sql script

--- task_4.sql
+++ task_4.sql
@@ -1,11 +1,11 @@
--- Full description of the table 'books' without using DESCRIBE or ANALYZE
+-- Full description of the table 'books' without using DESCRIBE, EXPLAIN, or ANALYZE
 SELECT 
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Data Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
   AND TABLE_NAME = 'books';
