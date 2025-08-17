8704565eb03c21c7423bb4dea349c1ebb8f37ee7
REBECCA-WONDU <your-email@example.com>
2025-08-17 12:12:35 +0300

    add task_6.sql script

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,11 @@
--- Insert multiple rows into the customer table
-USE alx_book_store;
+-- Insert customers 2, 3, and 4 into the currently selected database
+-- (the database will be passed as an argument to the mysql command)
 
 INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.');
+
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.');
+
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
