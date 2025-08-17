c16716a4fdb3578471d6896a4ff9a0ab8a447a45
REBECCA-WONDU <your-email@example.com>
2025-08-17 11:55:36 +0300

    add task_6.sql script

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,11 @@
--- Insert multiple customers into customers table
-INSERT INTO customers (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.');
-INSERT INTO customers (customer_id, customer_name, email, address) VALUES
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.');
-INSERT INTO customers (customer_id, customer_name, email, address) VALUES
-(4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+-- Insert customer with customer_id = 2
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.');
+
+-- Insert customer with customer_id = 3
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.');
+
+-- Insert customer with customer_id = 4
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
