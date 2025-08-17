600bc19c983253845a94c8216193061599d18ea1
REBECCA-WONDU <your-email@example.com>
2025-08-17 12:00:59 +0300

    add task_4.sql script

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,13 @@
--- Get full description of books table without using DESCRIBE/EXPLAIN
+-- task_4.sql
+-- Prints the full description of the table books from the given database
+
 SELECT 
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM 
-    information_schema.COLUMNS
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books';
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE()
+  AND TABLE_NAME = 'books'
+ORDER BY ORDINAL_POSITION;
