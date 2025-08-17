575e3dac7fa590d810dd3e1b9c389d52396c7c9e
REBECCA-WONDU <your-email@example.com>
2025-08-17 11:34:55 +0300

    add task_5.sql script

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+-- Insert single customer into customers table
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
