2e09188da533de425401f8c6b003f9f5a1c15c18
REBECCA-WONDU <your-email@example.com>
2025-08-17 12:05:03 +0300

    add task_3.sql script

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,3 @@
--- List all tables in alx_book_store database
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = 'alx_book_store';
+-- List all tables in the alx_book_store database
+USE alx_book_store;
+SHOW TABLES;
